862fe6424a95454208583598fd71fcb41f499365
lbarberiscanoni <hllbck7@gmail.com>
2015-03-12 16:59:29 -0700

    working on the responsive nav and video/image through js

--- scripts/mobileNav.js
+++ scripts/mobileNav.js
@@ -1,19 +1,23 @@
 $(window).ready(function(){
 	if ($(window).width() <= 500) {	
 		$("nav.full").hide();
+		$("nav.mobile").show();
 	}
 
 	if ($(window).width() >= 500) {
 		$("nav.full").show();
+		$("nav.mobile").hide();
 	}
 }); 
 
 $(window).resize(function(){
 	if ($(window).width() <= 500) {	
 		$("nav.full").hide();
+		$("nav.mobile").show();
 	}
 
 	if ($(window).width() >= 500) {
 		$("nav.full").show();
+		$("nav.mobile").hide();
 	}
 }); 
