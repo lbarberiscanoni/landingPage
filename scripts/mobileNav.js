1dbfacffc22847d9586e8d940ce1d6f676b9c76b
lbarberiscanoni <hllbck7@gmail.com>
2015-03-12 17:02:00 -0700

    swtiched the names for the scripts

--- scripts/mobileNav.js
+++ scripts/mobileNav.js
@@ -1,23 +1,3 @@
-$(window).ready(function(){
-	if ($(window).width() <= 500) {	
-		$("nav.full").hide();
-		$("nav.mobile").show();
-	}
-
-	if ($(window).width() >= 500) {
-		$("nav.full").show();
-		$("nav.mobile").hide();
-	}
-}); 
-
-$(window).resize(function(){
-	if ($(window).width() <= 500) {	
-		$("nav.full").hide();
-		$("nav.mobile").show();
-	}
-
-	if ($(window).width() >= 500) {
-		$("nav.full").show();
-		$("nav.mobile").hide();
-	}
-}); 
+$("button.menu").click(function(){
+	$("nav.navbar.mobile").toggle(400);
+});
--- scripts/responsiveMenu.js
+++ scripts/responsiveMenu.js
@@ -1,3 +1,23 @@
-$("button.menu").click(function(){
-	$("nav.navbar.mobile").toggle(400);
-});
+$(window).ready(function(){
+	if ($(window).width() <= 500) {	
+		$("nav.full").hide();
+		$("nav.mobile").show();
+	}
+
+	if ($(window).width() >= 500) {
+		$("nav.full").show();
+		$("nav.mobile").hide();
+	}
+}); 
+
+$(window).resize(function(){
+	if ($(window).width() <= 500) {	
+		$("nav.full").hide();
+		$("nav.mobile").show();
+	}
+
+	if ($(window).width() >= 500) {
+		$("nav.full").show();
+		$("nav.mobile").hide();
+	}
+}); 
